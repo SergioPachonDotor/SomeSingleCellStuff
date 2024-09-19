d3a47d1e0a9b2cc735cc50b4f2ad0fe59075becf
SergioPachonD <sap9827@gmail.com>
2024-09-18 19:52:38 -0500

    Minor changes

--- scripts/02_output_dirs_creator.py
+++ scripts/02_output_dirs_creator.py
@@ -16,25 +16,14 @@ def create_output_dirs(samples_dir:str, output_dir:str) -> None:
         os.makedirs(second_analysis_path, exist_ok=True)
         
         print(f"Directories created: {first_analysis_path}, {second_analysis_path}")
-
-# if __name__ == '__main__':
-#     # Samples and output directory
-#     samples_dir = "./samples"
-#     output_dir = "./output"
-#     # Call the function to create the directories
-#     create_output_dirs(samples_dir, output_dir)
     
 if __name__ == '__main__':
-    # Configurar el parser de argumentos
     parser = argparse.ArgumentParser(description="Create output directories for samples.")
     
-    # Agregar argumentos
     parser.add_argument('--samples_dir', type=str, required=True, help='Path to the samples directory')
     parser.add_argument('--output_dir', type=str, required=True, help='Path to the output directory')
 
-    # Parsear los argumentos
     args = parser.parse_args()
 
-    # Llamar a la función para crear los directorios con los argumentos recibidos
     create_output_dirs(args.samples_dir, args.output_dir)
 
